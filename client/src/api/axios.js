78bfea70b75cf4800470b5b40fbb01bcd75e9b00
archita-22 <architaverma10@gmail.com>
2026-07-29 13:00:55 +0530

    fix: implement dynamic vercel cors whitelisting and correct axios link

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://onrender.com"
+  baseURL: "https://nexthire-ai-4.onrender.com"
 });
 
 // Har request ke saath automatically token attach karega (agar login hai)
